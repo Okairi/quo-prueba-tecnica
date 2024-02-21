33a37bb7010919500330d641f20ac1a1d89bfbdf
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-20 23:30:11 -0500

    add services to list details banks

--- src/app/services/bankService.js
+++ src/app/services/bankService.js
@@ -3,3 +3,12 @@ import axios from "../utils/axios";
 export const getBanks = () => {
   return axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/institutions/`);
 };
+export const linksBank = (obj) => {
+  return this.http.post(`${process.env.NEXT_PUBLIC_BASE_URL}/links/`, obj);
+};
+
+export const transactionList = () => {
+  const ruta = `${environment.BASE_URL}/transactions/?page=1&link=${id}`;
+
+  return this.http.get(ruta);
+};
