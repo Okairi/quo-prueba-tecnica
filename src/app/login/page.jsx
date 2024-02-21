762a9f5ceef7bc45e47535de366a2ea451ec17d2
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-21 18:29:55 -0500

    Add protected route login register

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,17 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 import AuthPage from "../components/AuthPage";
+import { useLayoutEffect } from "react";
 
 function LoginPage() {
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    if (sessionStorage.getItem("idToken")) {
+      router.push("/");
+    }
+  }, []);
+
   return <AuthPage isLogin={true}></AuthPage>;
 }
 
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,17 @@
 "use client";
 import AuthPage from "../components/AuthPage";
+import { useRouter } from "next/navigation";
+import { useLayoutEffect } from "react";
 
 function RegisterPage() {
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    if (sessionStorage.getItem("idToken")) {
+      router.push("/");
+    }
+  }, []);
+
   return <AuthPage isLogin={false}></AuthPage>;
 }
 
