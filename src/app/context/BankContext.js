af2ddf2984cfad3fbeb89e89eff7671329e34263
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-22 13:37:59 -0500

    Add save context storage

--- src/app/context/BankContext.js
+++ src/app/context/BankContext.js
@@ -1,12 +1,18 @@
-// contexts/MyContext.js
-
 "use client";
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const MyContext = createContext();
 
 export function MyContextProvider({ children }) {
-  const [myState, setMyState] = useState({});
+  // Cargar el estado inicial desde localStorage, si existe
+  const initialState = JSON.parse(localStorage.getItem("myState")) || {};
+
+  const [myState, setMyState] = useState(initialState);
+
+  // Actualizar localStorage cada vez que myState cambie
+  useEffect(() => {
+    localStorage.setItem("myState", JSON.stringify(myState));
+  }, [myState]);
 
   const updateState = (newValue) => {
     setMyState(newValue);
