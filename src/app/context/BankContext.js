7ee313fc08498e0c46a4bfbfec781bddd4d66a2e
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-22 14:14:00 -0500

    fix kpi data

--- src/app/context/BankContext.js
+++ src/app/context/BankContext.js
@@ -4,12 +4,10 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const MyContext = createContext();
 
 export function MyContextProvider({ children }) {
-  // Cargar el estado inicial desde localStorage, si existe
   const initialState = JSON.parse(localStorage.getItem("myState")) || {};
 
   const [myState, setMyState] = useState(initialState);
 
-  // Actualizar localStorage cada vez que myState cambie
   useEffect(() => {
     localStorage.setItem("myState", JSON.stringify(myState));
   }, [myState]);
