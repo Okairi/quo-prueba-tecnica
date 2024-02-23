2c9f55b9733c6ec8cd2c6ad013f958e4e20f217c
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-22 23:56:26 -0500

    add style for list account

--- src/app/accounts/[id]/page.jsx
+++ src/app/accounts/[id]/page.jsx
@@ -4,26 +4,30 @@ import { accountsList } from "@/app/services/bankService";
 import { useEffect, useState } from "react";
 
 function AccountsPage({ params }) {
-  const [account, setaccount] = useState([]);
+  const [accounts, setAccounts] = useState([]);
 
   useEffect(() => {
-    const listAccount = async () => {
-      const listDataAccount = await accountsList(params.id);
-
-      console.log(listDataAccount.data.results);
-
-      setaccount(listDataAccount.data.results);
+    const fetchAccounts = async () => {
+      const accountData = await accountsList(params.id);
+      console.log(accountData.data.results);
+      setAccounts(accountData.data.results);
     };
-    listAccount();
-  }, []);
+    fetchAccounts();
+  }, [params.id]);
 
-  console.log(params);
   return (
-    <>
-      {account.map((val) => {
-        return <div key={val.id}>{val.name}</div>;
-      })}
-    </>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {accounts.map((account) => (
+          <div key={account.id}>
+            <div className="bg-white rounded-lg shadow-md p-4">
+              <h2 className="text-xl font-bold mb-2">{account.name}</h2>
+              <p className="text-gray-700">{account.currency}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
