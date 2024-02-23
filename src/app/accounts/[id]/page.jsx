34d916c5afc187d88b7dd22430dea09df593ff0a
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-23 00:17:33 -0500

    Add redirecto to transaction for account

--- src/app/accounts/[id]/page.jsx
+++ src/app/accounts/[id]/page.jsx
@@ -2,15 +2,38 @@
 
 import { accountsList } from "@/app/services/bankService";
 import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
+import { useMyContext } from "@/app/context/BankContext";
+import { SpinerLoading } from "@/app/components/SpinerLoading";
 
 function AccountsPage({ params }) {
   const [accounts, setAccounts] = useState([]);
+  const router = useRouter();
+  const { myState, updateState } = useMyContext();
 
+  const redirectTransactions = (id, name) => {
+    updateState({
+      id: id,
+      display_name: name,
+    });
+
+    router.push(`/transactions/${id}`);
+  };
   useEffect(() => {
     const fetchAccounts = async () => {
-      const accountData = await accountsList(params.id);
-      console.log(accountData.data.results);
-      setAccounts(accountData.data.results);
+      try {
+        const accountData = await accountsList(params.id);
+        console.log(accountData.data.results);
+        setAccounts(accountData.data.results);
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Hubo un error al cargar las cuentas.",
+        });
+        console.error(error);
+      }
     };
     fetchAccounts();
   }, [params.id]);
@@ -19,10 +42,16 @@ function AccountsPage({ params }) {
     <div className="container mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {accounts.map((account) => (
-          <div key={account.id}>
+          <div
+            className="cursor-pointer"
+            key={account.id}
+            onClick={() => {
+              redirectTransactions(account.link, account.institution.name);
+            }}
+          >
             <div className="bg-white rounded-lg shadow-md p-4">
               <h2 className="text-xl font-bold mb-2">{account.name}</h2>
-              <p className="text-gray-700">{account.currency}</p>
+              <p className="text-gray-700"> Divisa : {account.currency}</p>
             </div>
           </div>
         ))}
