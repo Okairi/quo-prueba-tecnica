f559a7abcbe9a6a8cd80bfb1218c7d8fdf9af8d5
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-23 00:23:37 -0500

    add loading for account list

--- src/app/accounts/[id]/page.jsx
+++ src/app/accounts/[id]/page.jsx
@@ -12,7 +12,10 @@ function AccountsPage({ params }) {
   const router = useRouter();
   const { myState, updateState } = useMyContext();
 
+  const [loading, setLoading] = useState(true);
+  SpinerLoading;
   const redirectTransactions = (id, name) => {
+    setLoading(true);
     updateState({
       id: id,
       display_name: name,
@@ -26,7 +29,9 @@ function AccountsPage({ params }) {
         const accountData = await accountsList(params.id);
         console.log(accountData.data.results);
         setAccounts(accountData.data.results);
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
         Swal.fire({
           icon: "error",
           title: "Error",
@@ -40,22 +45,26 @@ function AccountsPage({ params }) {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {accounts.map((account) => (
-          <div
-            className="cursor-pointer"
-            key={account.id}
-            onClick={() => {
-              redirectTransactions(account.link, account.institution.name);
-            }}
-          >
-            <div className="bg-white rounded-lg shadow-md p-4">
-              <h2 className="text-xl font-bold mb-2">{account.name}</h2>
-              <p className="text-gray-700"> Divisa : {account.currency}</p>
+      {loading ? ( // Mostrar el spinner de carga si loading es verdadero
+        <SpinerLoading />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {accounts.map((account) => (
+            <div
+              className="cursor-pointer"
+              key={account.id}
+              onClick={() => {
+                redirectTransactions(account.link, account.institution.name);
+              }}
+            >
+              <div className="bg-white rounded-lg shadow-md p-4">
+                <h2 className="text-xl font-bold mb-2">{account.name}</h2>
+                <p className="text-gray-700"> Divisa : {account.currency}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
